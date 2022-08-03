6bb687ea1dfb51b754a6c37e3cbf3ec9562c6b07
Антон <antonolegovich2017@icloud.com>
2022-08-03 17:38:24 +0600

    Домашка массивы 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] weights = {90, 92, 241, 42, 52, 25, 35, 0, 0, 0, 0, 0};
+       /* int[] weights = {90, 92, 241, 42, 52, 25, 35, 0, 0, 0, 0, 0};
         weights[0] = 90;
         int januaryWeights = weights[0];
         System.out.println(januaryWeights);
@@ -11,30 +11,58 @@ public class Main {
         System.out.println(weights[january]);
         for (int i = 0; i < weights.length; i++) {
             System.out.println(weights[i]);
-        }
+        }*/
 
         // Задание 1
-        int[] namber = new int[]{1, 2, 3, 4, 5, 6, 7,};
-        float[] fraction = new float[]{1.57f, 7.654f, 9.986f};
-        double[] freeArray = new double[]{1.3453463634676544, 2.345346346};
+        int[] ints = new int[3];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = i + 1;
+        }
+        double[] doubles = {1.57, 7.654, 9.986};
+        boolean[] booleans = {true, false};
 
         // Задание 2
-        System.out.println(namber[0] + "," + namber[1] + "," + namber[2]);
-        System.out.println(fraction[0] + "," + fraction[1] + "," + fraction[2]);
-        System.out.println(freeArray[0] + "," + freeArray[1]);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i]);
+            if (i != ints.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.print(doubles[i]);
+            if (i != doubles.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < booleans.length; i++) {
+            System.out.print(booleans[i]);
+            if (i != booleans.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
-        //Задание 3
-        System.out.println(namber[2] + "," + namber[1] + "," + namber[0]);
-        System.out.println(fraction[2] + "," + fraction[1] + "," + fraction[0]);
-        System.out.println(freeArray[1] + "," + freeArray[0]);
+        // Задание 3
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i]);
+            if (i != ints.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
-        // Задание 4
-        for (int a = namber[0]; a < namber[6]; a = a + 1) {
-            if ((a + 1) % 2 == 0) System.out.println(namber[a]);
-        }namber[2]= namber[2]+1;
-        namber[4]= namber[4] +1;
-        namber[6] = namber[6]+1;
     }
 }
+
+
+//Задание 3
+
+
+// Задание 4
+
+
+
 
 
